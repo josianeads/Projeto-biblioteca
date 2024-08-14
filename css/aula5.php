fb215e6598ffb841ffdc20bcc3b676f65bb472cb
josiane santos <josin_ha-78@hotmail.com>
2024-08-14 16:53:55 -0300

    array php

---
+++ css/aula5.php
@@ -0,0 +1,24 @@
+
+<?php
+ echo "<h1>Trabalhando com array em PHP</h1>";
+ 
+ echo "<h4>Array unidimensional</h4>";
+ 
+ //$frutas
+
+ $frutas= ['banana','maça','uva','laranja','tomate'];
+
+ echo $frutas;
+
+ echo"<hr>";
+
+ var_dump($frutas);
+
+ echo"<hr>";
+ echo"<pre>";
+
+ print_r($frutas);
+ echo "</pre>";
+ 
+?>
+ 
